75b55c5f2c420163363b2a578a504cbea42c2707
crlhenriq <crlhenriq@gmail.com>
2013-04-04 23:29:58 -0300

    projeto

---
+++ Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+	String nome;
+	String email;
+	int id;
+	usuario[] ListUsers;
+
+	 public int add(String nome, String email ,int id){
+
+	 	this.nome = nome;
+	 	this.email= email;
+	 	this.id=id;
+	 	return id;
+
+
+	 }
+	 
+}
